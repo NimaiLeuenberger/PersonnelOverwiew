8df543b0ab58c1f28e88d7539f43eb9d6a8c197d
Nimai Leuenberger <leuenbergern2@bzz.ch>
2022-06-15 10:53:15 +0200

    controller class added

---
+++ src/view/LoginView.java
@@ -0,0 +1,13 @@
+package view;
+
+import javax.swing.*;
+
+public class LoginView extends JFrame {
+    private JLabel function, pw;
+    private JTextField pwTextField;
+    private JComboBox functionComboBox;
+
+    public LoginView(){
+
+    }
+}
